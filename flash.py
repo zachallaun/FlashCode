36414f8bc1e2bdf0cd80478bce855c6c01200bcb
Zachary Allaun <zach.allaun@gmail.com>
2011-11-09 13:21:28 -0500

    Log to base file

--- flash.py
+++ flash.py
@@ -9,7 +9,7 @@ from countedconsole import CountedConsole
 # Assumes a folder called data/ in current dir
 teacher = Teacher('data/flashq.txt')
 # Assumes a folder called log/ in current dir
-sys.stdout = sys.stderr = sys.stdin = Watchman('log/flashlog.dat', teacher)
+sys.stdout = sys.stderr = sys.stdin = Watchman('flashlog.dat', teacher)
 
 # FlashCode banner and initial prompt
 welcome = "Welcome to FlashCode (FC), the interactive Python learning environment."
